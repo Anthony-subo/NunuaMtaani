4b6287d8cf3d0426c7729a1e4ee1a53b59d429b5
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-08-30 22:42:51 +0300

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // make sure root is current folder
+  build: {
+    outDir: 'dist', // where build goes
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // tell Vite where index.html is
+    },
+  },
 })
