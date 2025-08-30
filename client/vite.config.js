df6981901334549f3bd2cb5150561358ab634f19
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-08-30 22:44:36 +0300

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+// Equivalent of __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // make sure root is current folder
   build: {
-    outDir: 'dist', // where build goes
     rollupOptions: {
-      input: resolve(__dirname, 'index.html'), // tell Vite where index.html is
+      input: resolve(__dirname, 'index.html'), // explicitly point to index.html
     },
   },
 })
