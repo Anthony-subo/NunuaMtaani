4adac1eb49cee289a8d5eaea356021bd25b58eaf
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-09-02 19:18:53 +0300

    update

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2FsFm3381tK5W9e0Go3idb8VtEzngsTY",
-  authDomain: "project-8ac25.firebaseapp.com",
-  databaseURL: "https://project-8ac25-default-rtdb.firebaseio.com",
-  projectId: "project-8ac25",
-  storageBucket: "project-8ac25.appspot.com", // ✅ correct bucket
-  messagingSenderId: "622149030138",
-  appId: "1:622149030138:web:571686dc65367c508c7943"
-};
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
