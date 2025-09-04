677580763d8bb250d281e3a9f385f4f307f7ead1
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-09-04 13:10:04 +0300

    update

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Connect DB
 connectDB();
 
+
 // Routes
 app.use("/api/payments", require("./routes/payments"));
 app.use("/api/auth", require("./routes/authRoutes"));
