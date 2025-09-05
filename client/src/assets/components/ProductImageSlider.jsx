3276795aa89fcf34792358070cc1703ccf298419
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-09-05 11:33:03 +0300

    update

--- client/src/assets/components/ProductImageSlider.jsx
+++ client/src/assets/components/ProductImageSlider.jsx
@@ -4,9 +4,6 @@ import '../styles/productSlider.css';
 function ProductImageSlider({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Backend base URL from env
-  const API_URL = import.meta.env.VITE_API_URL;
-
   // Show at most 4 images
   const displayImages = images?.slice(0, 4) || [];
 
@@ -30,12 +27,12 @@ function ProductImageSlider({ images }) {
   };
 
   return (
-   <div className="slider-wrapper">
-  <img
-    src={product.images[currentIndex]}
-    className="slider-image"
-    alt={`Product image ${currentIndex + 1}`}
-  />
+    <div className="slider-wrapper">
+      <img
+        src={displayImages[currentIndex]}
+        className="slider-image"
+        alt={`Product image ${currentIndex + 1}`}
+      />
       {displayImages.length > 1 && (
         <>
           <button className="slider-btn left" onClick={prev}>‹</button>
