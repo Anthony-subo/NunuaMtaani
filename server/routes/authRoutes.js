afde0b644f734af7054378e4d986597d2b098dfb
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-09-07 12:14:39 +0300

    update

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // server/routes/authRoutes.js
 const express = require("express");
 const router = express.Router();
-const { register, login, verifyEmail } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
