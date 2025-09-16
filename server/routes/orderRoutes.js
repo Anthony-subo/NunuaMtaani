668190eb601ed520e9bc622ae5228b3c9136b28f
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2025-09-16 11:41:17 +0300

    update

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/orders');
-const express = require('express');
-const Order = require('../models/orders');
 const crypto = require('crypto');
 
 // ✅ POST /api/orders - create a new order
