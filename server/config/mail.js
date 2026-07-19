b164b3f6cfe92b947f3048cfe4e0d6b405572731
Anthony-subo <129326991+Anthony-subo@users.noreply.github.com>
2026-07-19 15:53:56 +0300

    update

--- server/config/mail.js
+++ server/config/mail.js
@@ -13,6 +13,8 @@ transporter.verify((error) => {
     console.error("❌ Email configuration error:");
     console.error(error);
   } else {
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
     console.log("✅ Email server is ready.");
     console.log(`📧 Sending emails from: ${process.env.EMAIL_USER}`);
   }
